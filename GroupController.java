f5d30ff9f55bf9cc42d4eec134e0109f222c778c
MayadaMohamed11 <36480871+MayadaMohamed11@users.noreply.github.com>
2019-12-24 20:45:22 +0200

    group class

---
+++ GroupController.java
@@ -0,0 +1,46 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author ahmed
+ */
+public class GroupController {
+
+    public ArrayList<post> postlist(String groupid) {
+
+    }
+
+    public void editstate(String groupid, String type) {
+
+    }
+
+    public void postornot(String groupid, boolean pornp) {
+
+    }
+
+    public void inviteuser(String userid) {
+
+    }
+
+    public void acceptuser(String userid) {
+
+    }
+
+    public void addadmin(User user, Group group) {
+
+    }
+
+    public void leavegroup(String userid) {
+
+    }
+
+    public void deletegroup(String groupid) {
+
+    }
+}
