726c20ca01fd85ab7a1a75943791f9d70cc7b8e2
MayadaMohamed11 <36480871+MayadaMohamed11@users.noreply.github.com>
2019-12-24 20:53:20 +0200

    add page controller class

---
+++ PageController.java
@@ -0,0 +1,40 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ahmed
+ */
+public class PageController {
+    public ArrayList<String> postlist(){
+         
+    }
+    public void addUser(String userId){
+        
+    }
+    public void addPost(Post post){
+        
+    }
+     public void addAdmin(User user,Page page){
+        
+    }
+     public void leavePage(String userId ){
+        
+    }
+     public boolean searchForMember(String userId){
+        
+    }
+     public int getNumOfAdmins(){
+        
+    }
+    public void deletePage(String pageId){
+        
+    } 
+    
+}
