cc8c4304ec69fb2e99d404e8e5f8720f122ff40d
mona010 <36086043+mona010@users.noreply.github.com>
2019-12-26 21:03:59 +0200

    update

--- FriendSearch.java
+++ FriendSearch.java
@@ -15,7 +15,27 @@ public class FriendSearch iimplements Search{
    
     @Override
     public ArrayList<String> search(String name) {
-        return null;
+        databasehandler db = new databasehandler();
+        AllUsers all = db.getalluser();
+        
+        ArrayList<User> nUsers = all.getNUser();
+        ArrayList<PremiumUser> pUsers = all.getPUser();
+        
+        ArrayList<String> matchIds = new ArrayList<String>();
+        
+        for (int i=0 ; i<nUsers.size() ; i++){
+            if (nUsers.get(i).getName().equals(name)){
+                matchIds.add(nUsers.get(i).getUserId());
+            }
+        }
+        
+        for (int i=0 ; i<pUsers.size() ; i++){
+            if (pUsers.get(i).getName().equals(name)){
+                matchIds.add(pUsers.get(i).getUserId());
+            }
+        }
+        
+        return matchIds;
     }
     
 }
