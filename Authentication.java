9c6ab692c04e8a33b3286738c492fe9fb80ab957
mona010 <36086043+mona010@users.noreply.github.com>
2019-12-26 21:03:06 +0200

    update login function

--- Authentication.java
+++ Authentication.java
@@ -9,9 +9,37 @@
  * @author DELL
  */
 public class Authentication {
-    public boolean logIn(String userName , String password){
-		return false;
-        //return true;
+    public boolean logIn(String userName , String password) {
+        databasehandler db = new databasehandler();
+        AllUsers all = db.getalluser();
+
+        ArrayList<User> nUsers = all.getNUser();
+        ArrayList<PremiumUser> pUsers = all.getPUser();
+
+        boolean normal = false;
+        boolean premium = false;
+        for (int i = 0; i < nUsers.size(); i++) {
+            if (nUsers.get(i).getName().equals(userName) && nUsers.get(i).getPassword().equals(password)) {
+                normal = true;
+                //System.out.println("Login Successfully...");
+            }
+        }
+
+        if (normal == false) {
+            for (int i = 0; i < pUsers.size(); i++) {
+                if (pUsers.get(i).getName().equals(userName) && pUsers.get(i).getPassword().equals(password)) {
+                    premium = true;
+                    //System.out.println("Login Successfully...");
+                }
+            }
+        }
+
+        if (normal = false && premium == false) {
+            //System.out.println("Login Failed...");
+            return false;
+        }
+        
+        return true;
     }
     
     public boolean logOut(){
