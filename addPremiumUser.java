6f9908538f245f57b88403362c024ebaa5b32f82
mona010 <36086043+mona010@users.noreply.github.com>
2019-12-26 21:02:14 +0200

    update

--- addPremiumUser.java
+++ addPremiumUser.java
@@ -9,8 +9,20 @@
  * @author DELL
  */
 public class addPremiumUser implements SignUp{
-    public boolean signUp(String userName , String email , String password , int phoneNmber){
-		return false;
-        //return true;
+    public String signUp(String userName, String email, String password, String gender, String country, Date birthdate){
+	PremiumUser pUser = new PremiumUser();
+        pUser.setName(userName);
+        pUser.setEmail(email);
+        pUser.setPassword(password);
+        pUser.setGender(gender);
+        pUser.setCountry(country);
+        pUser.setBirthdate(birthdate);
+        
+        databasehandler db = new databasehandler();
+        pUser = db.addpremiumuser(pUser);
+        
+        String newId = pUser.getUserId();
+        
+        return newId;
     }
 }
